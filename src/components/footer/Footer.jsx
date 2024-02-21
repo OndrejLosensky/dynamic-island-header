2486eed4fb9985e87b46b5b99826531a49725c35
Ondrej Losensky <ondra.losi@gmail.com>
2024-02-21 20:57:35 +0100

    footer / width

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="w-[68%] mx-auto h-[50px] py-2 bg-customGray"style={{borderTopLeftRadius: '35px', borderTopRightRadius: '35px'}}>
+    <div className="w-[58%] mx-auto h-[50px] py-2 bg-customGray"style={{borderTopLeftRadius: '35px', borderTopRightRadius: '35px'}}>
             <h3 className="text-md pt-2 text-customWhite text-center">
                  Created by Losenský Ondřej | ⭐️ Star this project if you like it!
             </h3>
