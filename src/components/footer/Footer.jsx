53aab44ad77f22d5cf2a9b1df3319cee934ed274
Ondrej Losensky <ondra.losi@gmail.com>
2024-02-27 14:54:10 +0100

    added hover effect over link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="w-[58%] mx-auto h-[50px] py-2 bg-customGray"style={{borderTopLeftRadius: '35px', borderTopRightRadius: '35px'}}>
             <h3 className="text-md pt-2 text-customWhite text-center">
-                 Created by <a className='font-bold underline-offset-2 underline' href="https://github.com/OndrejLosensky">Ondřej Losenský</a> | ⭐️ Star this project if you like it!
+                 Created by <a className='font-bold underline-offset-2 underline hover:text-customBlue duration-200' href="https://github.com/OndrejLosensky">Ondřej Losenský</a> | ⭐️ Star this project if you like it!
             </h3>
     </div>
   )
